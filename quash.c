4712ee5f03ead807542d1a70cc6b17fb92d34ff7
prathap.kumarvalsan <kvprathap@gmail.com>
2014-10-08 10:36:43 -0500

    Working shell. Added signal handling and Job management.

--- quash.c
+++ quash.c
@@ -12,6 +12,15 @@
 #define TRUE 1
 #define FALSE 0
 #define MAXJOB 20
+#define SUSPENDED 'S'
+#define WAITING_INPUT 'W'
+#define STDIN 1
+#define STDOUT 2
+
+
+#define BY_PROCESS_ID 1
+#define BY_JOB_ID 2
+#define BY_JOB_STATUS 3
 static char cmd_buffer[BMAX];
 static char* myargv[5];
 static int myargc = 0;
@@ -21,41 +30,28 @@ static int pos;
 static char* cwd;
 typedef void (*sighandler_t)(int);
 int status;
-int fdout,fdin;	
-
-/* Structure to store a particular job */
-struct job{
-pid_t pid;
-int jid;
-int state;
-char *input;
-};
-
-/* Structure contains list of jobs */
-struct job list_of_jobs[MAXJOB]; 
+int fdout,fdin;
+static int QUASH_TERMINAL;
+static int numActiveJobs = 0;
+static char* currentDirectory;
+static pid_t QUASH_PID;
+static pid_t QUASH_PGID;
+static int QUASH_TERMINAL, QUASH_IS_INTERACTIVE;
+static struct termios QUASH_TMODES;
 
 
 
-/* function for clearing all jobs in the list */
-void clear_jobs(struct job *job){
-	job->pid=0;
-	job->jid=0;
-	job->state=0;
-	job->input='\0';
-}
+typedef struct job {
+        int id;
+        char *name;
+        pid_t pid;
+        pid_t pgid;
+        int status;
+        char *descriptor;
+        struct job *next;
+} t_job;
 
-/* function for initialisation of job list */
-void joblist_initialization(struct job *list_of_jobs){
-	int temp;
-        while(temp < MAXJOB){
-		clear_jobs(&list_of_jobs[temp]);
-	}
-}
-
-
-
-
-
+static t_job* jobsList = NULL;
 
 typedef enum execution_mode
 {
@@ -77,28 +73,90 @@ void shell_display()
 
 void signal_handler(int signo)
 {
-	shell_display();
 	fflush(stdout);
+	shell_display();
 }
-
-
-
-/* Find a job from a given job ID or name */
-struct job *get_job(struct job *list_of_jobs,pid_t pid){
-	int temp;
-	for(temp=0;temp<MAXJOB;temp++){
-		if(list_of_jobs[temp].pid== pid){
-			return &list_of_jobs[temp];
-		}
-	}
-	return &list_of_jobs[temp];
-}
-
-
-void killing_job(struct job *list_of_jobs, int pid)
+t_job* delJob(t_job* job)
 {
-        struct job *jobname = get_job(list_of_jobs,pid);
-        kill(jobname->pid, SIGKILL);
+        usleep(10000);
+        if (jobsList == NULL)
+                return NULL;
+        t_job* currentJob;
+        t_job* beforeCurrentJob;
+
+        currentJob = jobsList->next;
+        beforeCurrentJob = jobsList;
+
+        if (beforeCurrentJob->pid == job->pid) {
+
+                beforeCurrentJob = beforeCurrentJob->next;
+                numActiveJobs--;
+                return currentJob;
+        }
+
+        while (currentJob != NULL) {
+                if (currentJob->pid == job->pid) {
+                        numActiveJobs--;
+                        beforeCurrentJob->next = currentJob->next;
+                }
+                beforeCurrentJob = currentJob;
+                currentJob = currentJob->next;
+        }
+        return jobsList;
+}
+
+
+void waitjob(t_job* job)
+{
+        int terminationStatus; 
+        while (waitpid(job->pid, &terminationStatus, WNOHANG) == 0) {
+                if (job->status == SUSPENDED)
+                        return;
+        }
+        jobsList = delJob(job);
+}
+
+t_job* getjob(int searchValue, int searchParameter)
+{
+        usleep(10000);
+        t_job* job = jobsList;
+        switch (searchParameter) {
+        case BY_PROCESS_ID:
+                while (job != NULL) {
+                        if (job->pid == searchValue)
+                                return job;
+                        else
+                                job = job->next;
+                }
+                break;
+        case BY_JOB_ID:
+                while (job != NULL) {
+                        if (job->id == searchValue)
+                                return job;
+                        else
+                                job = job->next;
+                }
+                break;
+        case BY_JOB_STATUS:
+                while (job != NULL) {
+                        if (job->status == searchValue)
+                                return job;
+                        else
+                                job = job->next;
+                }
+                break;
+        default:
+                return NULL;
+                break;
+        }
+        return NULL;
+}
+
+void killjob(int jobId)
+{
+        t_job *job = getjob(jobId, BY_JOB_ID);
+	printf("pid = %d\n",job->pid);
+        kill(job->pid, SIGKILL);
 }
 
 
@@ -150,6 +208,31 @@ int check_for_symbol(char* token)
 	return 0;
 }
 
+
+void display_jobs()
+{
+        printf("\nActive jobs:\n");
+        printf(
+                "---------------------------------------------------------------------------\n");
+        printf("| %7s  | %30s | %5s | %10s | %6s |\n", "job no.", "name", "pid",
+               "descriptor", "status");
+        printf(
+                "---------------------------------------------------------------------------\n");
+        t_job* job = jobsList;
+        if (job == NULL) {
+                printf("| %s %62s |\n", "No Jobs.", "");
+        } else {
+                while (job != NULL) {
+                        printf("|  %7d | %30s | %5d | %10s | %6c |\n", job->id, job->name,
+                               job->pid, job->descriptor, job->status);
+                        job = job->next;
+                }
+        }
+        printf(
+                "---------------------------------------------------------------------------\n");
+}
+
+
 void change_directory()
 {
         if (myargv[1] == NULL) {
@@ -159,21 +242,6 @@ void change_directory()
                         printf(" %s: no such directory\n", myargv[1]);
                 }
         }
-}
-
-void display_jobs(struct job *list_of_jobs){
-
-
-	printf("\nActive jobs in under quash \n");
-        printf("jid      pid      status    \n");
-        
-        int temp = 0;
-	for (temp=0;temp<MAXJOB;temp++){
-        	if (list_of_jobs[temp].pid != 0) {
-                printf("%d \t %d \t %d \n",list_of_jobs[temp].jid,list_of_jobs[temp].pid,list_of_jobs[temp].state);
-        	} 
-	}
-        
 }
 
 
@@ -193,94 +261,245 @@ int parse_for_shell_commands()
         }
 	
 	if (strcmp("jobs", myargv[0]) == 0) { // Printing all jobs
-                display_jobs(list_of_jobs);
+                display_jobs();
 		return 1;
         }
 	
 	if (strcmp("kill", myargv[0]) == 0) { // Killing a job
                 
 		int jobid = atoi(myargv[1]);
-                killing_job(list_of_jobs , jobid);
+		killjob(jobid);
                 return 1;
         }
+
+	/*TODO:Set*/
     	return 0;
 
 
 	
+} 
+
+void putJobForeground(t_job* job, int continueJob)
+{
+        job->status = FG;
+        tcsetpgrp(QUASH_TERMINAL, job->pgid);
+        if (continueJob) {
+                if (kill(-job->pgid, SIGCONT) < 0)
+                        perror("kill (SIGCONT)");
+        }
+
+        waitjob(job);
+        tcsetpgrp(QUASH_TERMINAL, QUASH_PGID);
+}
+
+void putJobBackground(t_job* job, int continueJob)
+{
+        if (job == NULL)
+                return;
+
+        if (continueJob && job->status != WAITING_INPUT)
+                job->status = WAITING_INPUT;
+        if (continueJob)
+                if (kill(-job->pgid, SIGCONT) < 0)
+                        perror("kill (SIGCONT)");
+
+        tcsetpgrp(QUASH_TERMINAL, QUASH_PGID);
+}
+
+t_job* insertjob(pid_t pid, pid_t pgid, char* name, char* descriptor,
+                 int status)
+{
+        usleep(10000);
+        t_job *newJob = malloc(sizeof(t_job));
+
+        newJob->name = (char*) malloc(sizeof(name));
+        newJob->name = strcpy(newJob->name, name);
+        newJob->pid = pid;
+        newJob->pgid = pgid;
+        newJob->status = status;
+        newJob->descriptor = (char*) malloc(sizeof(descriptor));
+        newJob->descriptor = strcpy(newJob->descriptor, descriptor);
+        newJob->next = NULL;
+
+        if (jobsList == NULL) {
+                numActiveJobs++;
+                newJob->id = numActiveJobs;
+                return newJob;
+        } else {
+                t_job *auxNode = jobsList;
+                while (auxNode->next != NULL) {
+                        auxNode = auxNode->next;
+                }
+                newJob->id = auxNode->id + 1;
+                auxNode->next = newJob;
+                numActiveJobs++;
+                return jobsList;
+        }
 }
 
 void execute_command (char *command[],mode mode)
 {
-	int pid1;
-	pid1 = fork();
-	if (pid1 == 0) {
 		close(fdin);
 		close(fdout);
 		if (execvp(*command, command) == -1)
                 	perror("quash");
-		exit(0);	
-	}
-	else {
-		waitpid(pid1, &status,0);
-	}
 }
 
-void start_job()
+int changeJobStatus(int pid, int status)
+{
+        usleep(10000);
+        t_job *job = jobsList;
+        if (job == NULL) {
+                return 0;
+        } else {
+                int counter = 0;
+                while (job != NULL) {
+                        if (job->pid == pid) {
+                                job->status = status;
+                                return TRUE;
+                        }
+                        counter++;
+                        job = job->next;
+                }
+                return FALSE;
+        }
+}
+
+void signalHandler_child(int p)
+{
+        pid_t pid;
+        int terminationStatus;
+        pid = waitpid(WAIT_ANY, &terminationStatus, WUNTRACED | WNOHANG);
+        if (pid > 0) {
+                t_job* job = getjob(pid, BY_PROCESS_ID);
+                if (job == NULL)
+                        return;
+                if (WIFEXITED(terminationStatus)) {
+                        if (job->status == BG) {
+                                printf("\n[%d]+  Done\t   %s\n", job->id, job->name);
+                                jobsList = delJob(job);
+                        }
+                } else if (WIFSIGNALED(terminationStatus)) {
+                        printf("\n[%d]+  KILLED\t   %s\n", job->id, job->name);
+                        jobsList = delJob(job);
+                } else if (WIFSTOPPED(terminationStatus)) {
+                        if (job->status == BG) {
+                                tcsetpgrp(QUASH_TERMINAL, QUASH_PGID);
+                                changeJobStatus(pid, WAITING_INPUT);
+                                printf("\n[%d]+   suspended [wants input]\t   %s\n",
+                                       numActiveJobs, job->name);
+                        } else {
+                                tcsetpgrp(QUASH_TERMINAL, job->pgid);
+                                changeJobStatus(pid, SUSPENDED);
+                                printf("\n[%d]+   stopped\t   %s\n", numActiveJobs, job->name);
+                        }
+                        return;
+                } else {
+                        if (job->status == BG) {
+                                jobsList = delJob(job);
+                        }
+                }
+                tcsetpgrp(QUASH_TERMINAL, QUASH_PGID);
+        }
+}
+
+
+void init()
+{
+        QUASH_PID = getpid();
+        QUASH_TERMINAL = STDIN_FILENO;
+        QUASH_IS_INTERACTIVE = isatty(QUASH_TERMINAL);
+
+        if (QUASH_IS_INTERACTIVE) {
+                while (tcgetpgrp(QUASH_TERMINAL) != (QUASH_PGID = getpgrp()))
+                        kill(QUASH_PID, SIGTTIN);
+
+                signal(SIGQUIT, SIG_IGN);
+                signal(SIGTTOU, SIG_IGN);
+                signal(SIGTTIN, SIG_IGN);
+                signal(SIGTSTP, SIG_IGN);
+                signal(SIGINT, SIG_IGN);
+                signal(SIGCHLD, &signalHandler_child);
+
+                setpgid(QUASH_PID, QUASH_PID);
+                QUASH_PGID = getpgrp();
+                if (QUASH_PID != QUASH_PGID) {
+                        printf("Error, the shell is not process group leader");
+                        exit(EXIT_FAILURE);
+                }
+                if (tcsetpgrp(QUASH_TERMINAL, QUASH_PGID) == -1)
+                        tcgetattr(QUASH_TERMINAL, &QUASH_TMODES);
+
+                currentDirectory = (char*) calloc(1024, sizeof(char));
+        } else {
+                printf("Could not make QUASH interactive. Exiting..\n");
+                exit(EXIT_FAILURE);
+        }
+                shell_display();
+                fflush(stdout);
+}
+
+
+void start_job(char *command[], char *file)
 {
 	char* filename = NULL;
 	pid_t pid;
-	mode mode = 0;
 	redirection redirection = 0;
-
+	mode mode = 0;
 	mode  = check_for_symbol("&");
 	redirection = check_for_symbol('\0');
+        /*TODO: check_for_symbol('|') and Implement pipeline*/
 	pid = fork();
 	if (pid == 0) {
+		signal(SIGINT, SIG_DFL);
+                signal(SIGQUIT, SIG_DFL);
+                signal(SIGTSTP, SIG_DFL);
+                signal(SIGCHLD, &signalHandler_child);
+                signal(SIGTTIN, SIG_DFL);
+                usleep(20000);
+		setpgrp();
+		if (mode == FG)
+			tcsetpgrp(QUASH_TERMINAL, getpid());
+		if (mode == BG)
+			 printf("[1] %d\n",(int) getpid());
 		if (redirection == WRITE)
 		{
-			filename = myargv[pos + 1];
-			myargv[pos] = '\0';
+			filename = command[pos + 1];
+			command[pos] = '\0';
 			printf("%s\n",filename); 
 			fdout = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 			dup2(fdout, STDOUT_FILENO);
-          		execute_command(myargv,mode);
+          		execute_command(command, mode);
           		close(fdout);
           		exit(0);
         	}
+
+		/*TODO: READ*/
+		else if(redirection == READ) {
+		}
 			
 		else {
-			execute_command(myargv,mode);
+          		execute_command(command, mode);
 			exit(0);
 		}
 	}
 	else {
-		if (mode ==  BG)
-			printf("[1] %d\n",pid);
-		waitpid(pid,&status, 0);
+		setpgid(pid, pid);
+		jobsList = insertjob(pid, pid, *(command), file, (int)mode);
+		t_job *job = getjob(pid, BY_PROCESS_ID);
+		if (mode == FG) {
+			putJobForeground(job, FALSE);
+		}
+		if (mode == BG)
+			putJobBackground(job, FALSE);
 	}
 }
 
 void handle_command()
 {
 	if (parse_for_shell_commands() == 0) {
-		start_job();
+		start_job(myargv, "STANDARD");
 	}
-}
-
-
-
-void initialize()
-{
-        
-        
-                signal(SIGQUIT, SIG_IGN);
-                signal(SIGTTOU, SIG_IGN);
-                signal(SIGTTIN, SIG_IGN);
-                signal(SIGTSTP, SIG_IGN);
-                signal(SIGINT, SIG_IGN);
-                shell_display();
-                fflush(stdout);
-                
 }
 
 
@@ -288,7 +507,7 @@ void initialize()
 int main(int argc, char *argv[], char *envp[])
 {	
 	
-	initialize();
+	init();
 	while(TRUE) {
 		input = getchar();
 		switch(input) {
